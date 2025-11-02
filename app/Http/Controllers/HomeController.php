4e089888e851f304d1d15766d5b933b91af1c37d
Patrick <patryk3580@gmail.com>
2025-11-02 21:15:20 +0100

    feat: add configurable system settings management

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Service;
+use App\Support\Settings\SettingsManager;
 
 class HomeController extends Controller
 {
+    public function __construct(private readonly SettingsManager $settings)
+    {
+    }
+
     public function index()
     {
         $services = Service::active()
             ->ordered()
             ->get();
 
-        return view('home', compact('services'));
+        return view('home', [
+            'services' => $services,
+            'marketingContent' => $this->settings->marketingContent(),
+        ]);
     }
 }
